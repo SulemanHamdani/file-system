9600c5fb64b83c1d1b64db75ff3ea34468793f5f
SulemanHamdani <96333810+SulemanHamdani@users.noreply.github.com>
2022-10-21 20:33:59 +0500

    Add files via upload

---
+++ index.py
@@ -0,0 +1,17 @@
+from lib import File, Folder
+
+# Data initializaion
+root = Folder("~")
+images = Folder("images")
+
+suleman = File("suleman.txt", '~/images/')
+farhan = File("farhan.txt", '~/images/')
+suleman.update_content('suleman')
+farhan.update_content('farhan')
+
+root.add(images)
+images.add(farhan)
+images.add(suleman)
+
+print(len(root.children))
+print(len(images.children))
