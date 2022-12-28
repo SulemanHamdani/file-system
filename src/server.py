67148d464fe5729959f488b83776aa062c988a04
Subhan <msabbas.bscs20seecs@student.nust.edu.pk>
2022-12-28 19:39:28 +0500

    Cleanup

--- src/server.py
+++ src/server.py
@@ -1,13 +1,14 @@
 import socket
-import threading
-
 from _thread import start_new_thread
 from FileSystem import FileManager, User
 from execute import execute_command
 
 HOST = ""
 PORT = 65432
+MAX_CONNECTIONS = 5
 host_name = socket.gethostname()
+host_ip = socket.gethostbyname(host_name)
+
 file_manager = FileManager()
 # thread function
 def threaded(conn):
@@ -39,16 +40,13 @@ def main():
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.bind((HOST, PORT))
     # put the socket into listening mode
-    s.listen(5)
-    print(f"Listening for connections on port {PORT}...")
+    s.listen(MAX_CONNECTIONS)
+    print(f"Listening for connections. Device IP: {host_ip}")
     # a forever loop until client wants to exit
     while True:
         # establish connection with client
         conn, addr = s.accept()
         print("Connected to :", addr[0], ":", addr[1])
         start_new_thread(threaded, (conn,))
-
-    s.close()
-
 
 main()
